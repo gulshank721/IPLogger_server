b79f6068c76f6fe75970312b3cb51e49d3ef7622
Gulshan Kumar <gulshank721@gmail.com>
2025-08-09 02:07:43 +0530

    added server url

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports ={
     'mongoUrl' : 'mongodb+srv://gulshank721:txno9211@cluster0.eyxv6c7.mongodb.net/IPLogger?retryWrites=true&w=majority',
     'ipInfo_token' : '6d5b5cb95e14c0', 
-    'server_url' : 'https://iplogger-server.onrender.com/',
+    'server_url' : 'https://iplogger-server.onrender.com',
 }
